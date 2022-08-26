8d6c4e8ad08ffbd56d392c9b58469348a4a5563d
Colibri Shin <colibrishin@gmail.com>
2022-08-26 18:12:51 +0900

    Writing XMLDocs

--- Libplanet/Crypto/IECPrivateKey.cs
+++ Libplanet/Crypto/IECPrivateKey.cs
@@ -3,13 +3,36 @@ using Org.BouncyCastle.Crypto.Parameters;
 
 namespace Libplanet.Crypto
 {
+    /// <summary>
+    /// The interface of Elliptic Curve Private Key. It allows to align the essential methods and
+    /// uses in <see cref="CryptoConfig"/> and <see cref="ICryptoBackend{T}"/> , mainly
+    /// <see cref="Sign"/> and <see cref="KeyBytes"/>. This should be inherited if there's a new
+    /// implementation for Elliptic Curve private key.
+    /// </summary>
     // ReSharper disable once InconsistentNaming
     public interface IECPrivateKey
     {
+        /// <summary>
+        /// The <see cref="byte"/> representation of private key.
+        /// </summary>
         IReadOnlyList<byte> KeyBytes { get; }
 
+        /// <summary>
+        /// The key parameter, <see cref="ECPrivateKeyParameters"/> of private key.
+        /// </summary>
         ECPrivateKeyParameters KeyParam { get; }
 
+        /// <summary>
+        /// The public key pair, <see cref="IECPublicKey"/> of private key.
+        /// </summary>
+        IECPublicKey PublicKey { get; }
+
+        /// <summary>
+        /// Signs the message with base private key method. See base sign method for detailed
+        /// description about signing and possibly thrown exceptions.
+        /// </summary>
+        /// <param name="message">A message to sign.</param>
+        /// <returns>Returns a <see cref="byte"/> array of signature.</returns>
         public byte[] Sign(byte[] message);
     }
 }
--- Libplanet/Crypto/IECPublicKey.cs
+++ Libplanet/Crypto/IECPublicKey.cs
@@ -3,13 +3,34 @@ using Org.BouncyCastle.Crypto.Parameters;
 
 namespace Libplanet.Crypto
 {
+    /// <summary>
+    /// The interface of Elliptic Curve Public Key. It allows to align the essential methods and
+    /// uses in <see cref="CryptoConfig"/> and <see cref="ICryptoBackend{T}"/>, mainly
+    /// <see cref="Verify"/> and <see cref="KeyBytes"/>. This should be
+    /// inherited if there's a new implementation for Elliptic Curve Public Key.
+    /// </summary>
     // ReSharper disable once InconsistentNaming
     public interface IECPublicKey
     {
+        /// <summary>
+        /// The <see cref="byte"/> representation of public key.
+        /// </summary>
         IReadOnlyList<byte> KeyBytes { get; }
 
+        /// <summary>
+        /// The key parameter, <see cref="ECPublicKeyParameters"/> of public key.
+        /// </summary>
         ECPublicKeyParameters KeyParam { get; }
 
+        /// <summary>
+        /// Verifies message with base public key method. See base verify method for detailed
+        /// description about signing and possibly thrown exceptions.
+        /// </summary>
+        /// <param name="message">A message to verify.</param>
+        /// <param name="signature">A signature to verify.</param>
+        /// <returns>Returns <see langword="true"/> if given <paramref name="message"/> and
+        /// <paramref name="signature"/> is valid, otherwise returns <see langword="false"/>.
+        /// </returns>
         public bool Verify(IReadOnlyList<byte> message, IReadOnlyList<byte> signature);
     }
 }
