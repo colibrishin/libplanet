7abba87893c57a3eaa10dac919f8f7f0d5f3c675
Colibri Shin <colibrishin@gmail.com>
2023-01-17 14:07:37 +0900

    test: closing connect() failed socket

--- Libplanet.Net.Tests/Transports/NetMQTransportTest.cs
+++ Libplanet.Net.Tests/Transports/NetMQTransportTest.cs
@@ -1,6 +1,11 @@
 #nullable disable
 using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using Libplanet.Crypto;
+using Libplanet.Net.Messages;
 using Libplanet.Net.Transports;
 using NetMQ;
 using Serilog;
@@ -41,6 +46,59 @@ namespace Libplanet.Net.Tests.Transports
         ~NetMQTransportTest()
         {
             Dispose(false);
+        }
+
+        [Fact(Timeout = Timeout)]
+        public async Task CloseConnectionFailedManually()
+        {
+            // This is a bug of NetMQ.
+            // Limits the number of sockets for shortening the test.
+            NetMQConfig.MaxSockets = 100;
+            ITransport transport = TransportConstructor.Invoke(
+                null,
+                new AppProtocolVersionOptions(),
+                new HostOptions(IPAddress.Loopback.ToString(), new IceServer[] { }),
+                null);
+
+            try
+            {
+                await InitializeAsync(transport);
+                var tasks = Enumerable.Range(0, 200).Select(
+                    async x =>
+                    {
+                        // Throttle the request to avoid simultaneous socket allocation.
+                        await Task.Delay(500 + (x * 10));
+                        return transport.SendMessageAsync(
+                            new BoundPeer(
+                                new PrivateKey().PublicKey,
+                                new DnsEndPoint(
+                                    "test.nowhere.doesnt.exist",
+                                    1234
+                                )
+                            ),
+                            new PingMsg(),
+                            TimeSpan.FromSeconds(1),
+                            CancellationToken.None);
+                    }).ToArray();
+
+                Task.WaitAll(tasks.ToArray());
+
+                Assert.All(tasks, task =>
+                {
+                    task.Result.Exception?.Handle(ex =>
+                    {
+                        // This thrown when the socket allocation has been failed
+                        // before the connection.
+                        Assert.IsType<CommunicationFailException>(ex);
+                        Assert.IsNotType<NetMQException>(ex.InnerException);
+                        return true;
+                    });
+                });
+            }
+            finally
+            {
+                transport.Dispose();
+            }
         }
 
         public void Dispose()
